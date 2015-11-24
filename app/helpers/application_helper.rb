782b9df9e9cd799439ad3c2dd2fd12e29f9db5f5
Junko Werner <aliciajace@gmail.com>
2015-11-23 19:38:21 -0800

    Post validation, Rspec, content_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	#  Any public method we write in ApplicationHelper will be available in all views.
+
+	def form_group_tag(errors, &block)
+		# The & turns the block into a Proc, a block that can be reused like a variable.
+		if errors.any?
+			content_tag :div, capture(&block), class: 'form-group has-error'
+		else
+			content_tag :div, capture(&block), class: 'form-group'
+		end
+	end
 end
